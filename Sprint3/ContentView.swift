5f57e4b52cc2f9b7e94f1c397f05656e7f0e8cf3
OMRM <oscarmrm14@gmail.com>
2024-05-18 13:34:49 +0200

    Initial Commit

---
+++ Sprint3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sprint3
+//
+//  Created by Óscar M on 18/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sprint3/Sprint3App.swift
@@ -0,0 +1,17 @@
+//
+//  Sprint3App.swift
+//  Sprint3
+//
+//  Created by Óscar M on 18/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Sprint3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
